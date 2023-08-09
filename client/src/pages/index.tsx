54783607ca45515c9d6ef16547bdf83d9aac312f
Becki Wordsworth <becki@hiotlabs.com>
2023-08-10 01:46:12 +0200

    Adding styling to the new home page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,26 +1,26 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import styled from 'styled-components'
 
 export default function Index() {
   let userLogged = null;
     return (
       <div className="homepage">
 
-        <div className="hero">
-          <div className="hero-icon"><img src="/logo.png" alt="Logo" /></div>
-          <div className="hero-content">
+        <Hero>
+          <HeroIcon><img src="/logo.png" alt="Logo" /></HeroIcon>
+          <HeroContent>
             <h1>DuoPhysics</h1>
             <p>Learn Physics for free</p>
             <br />
             <a href={userLogged}>Get Started</a>
-          </div>
-        </div>
+          </HeroContent>
+        </Hero>
 
         <section>
           <div className="section-content">
             <h2>A fun way to learn Physics</h2>
             <p>Learning with DuoPhysics is fun! Earn points for correct answers, collect badges, and level up. This bite-sized lessons are effective, and we have proof that it works. We have a range of subjects for your to dive into which are both fun and interesting.</p>
-            <div className="Science">
+            <ScienceIcon>
                 <img src="/science-book.png" alt="sciencebook" />
                 <img src="/dropper.png" alt="dropper" />
                 <img src="/nuclear.png" alt="nuclear" />
@@ -31,27 +31,107 @@ export default function Index() {
                 <img src="/flask.png" alt="flask" />
                 <img src="/newton-cradle.png" alt="newton" />
                 <img src="/space.png" alt="space" />
-            </div>
+            </ScienceIcon>
             </div>
         </section>
 
-        <section>
-          <div className="section-content">
+        <Section>
+         <section>
+          <SectionContent>
             <h2>Learn anywhere</h2>
             <p>Make your breaks and commutes more productive by learning through your phone aswell. Lessons are quick and can be played anywhere.</p>
-            <div className="image-icon">
+            <ImageIcon>
                 <img src="/smartphone.png" alt="phone" />
                 <img src="/tablet.png" alt="tablet" />
                 <img src="/laptop.png" alt="laptop" />
-            </div>
-          </div>
-        </section>
+            </ImageIcon>
+           </SectionContent>
+          </section>
+        </Section>
 
-        <footer>
-          <div className="footer-content">
+        <Footer>
+          <FooterContent>
             Learning Physics the fun way with DuoPhysics.
-          </div>
-        </footer>
+          </FooterContent>
+        </Footer>
       </div>
     )
   }
+
+
+const Hero = styled.div`
+    background-color: #1866a6;
+    height: 500px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+`;
+
+const HeroIcon = styled.div`
+    width: 200px;
+`;
+
+const HeroContent = styled.div`
+    color: white;
+    font-size: 18pt;
+    font-weight: bold;
+    text-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+    text-align: center;
+
+    a {
+        padding: 10px 50px;
+        background-color: #22aa33;
+        border-radius: 30px;
+        color: white;
+        text-decoration: none;
+        font-size: 14pt;
+    }
+
+    a:hover {
+     background-color: #229922;
+    }
+`;
+
+const Section = styled.div`
+  section {
+    background-color: #ddd;
+    padding: 20px;
+  }
+
+  section:nth-child(odd) {
+    background-color: #f3f3f3;
+  }
+`;
+
+const SectionContent = styled.div`
+    max-width: 600px;
+    margin: 0 auto;
+`;
+
+const Footer = styled.div`
+    background-color: #1866a6;
+    min-height: 100px;
+    padding: 40px 20px;
+`;
+
+const FooterContent = styled.div`
+    max-width: 600px;
+    margin: 0 auto;
+    color: #ddd;
+`;
+
+const ScienceIcon = styled.div`
+    img {
+    margin: 10px;
+    }
+`;
+
+const ImageIcon = styled.div`
+    margin: 0 auto;
+  
+    img {
+     height: 50px;
+    }
+`;
+
