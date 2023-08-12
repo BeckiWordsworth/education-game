bc5a868c800f8d8dcd91ecf46af3b12779cfe2f3
Becki Wordsworth <becki@hiotlabs.com>
2023-08-12 23:49:49 +0200

    Adding all paths we will need

--- server/constants/Paths.ts
+++ server/constants/Paths.ts
@@ -1,2 +1,17 @@
 export const CREATE_USER = "/user";
-export const GET_USER = "/user"
+export const GET_USER = "/user";
+export const UPDATE_USER = "/user";
+export const DELETE_USER = "/user";
+
+export const GET_TOPIC = "/topics";
+export const CREATE_TOPIC = "/topics";
+export const UPDATE_TOPIC = "/topics";
+export const DELETE_TOPIC = "/topics";
+
+export const GET_QUESTION = "/questions";
+export const CREATE_QUESTION = "/questions";
+export const UPDATE_QUESTION = "/questions";
+export const DELETE_QUESTION = "/questions";
+
+export const GET_RESULT = "/results";
+export const DELETE_RESULT = "/results";
